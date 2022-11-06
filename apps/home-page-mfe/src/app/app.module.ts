4b248a1cea3ba0dc2c553dde9487951009386d8d
Hüseyin Altun <altun.huseyin@outlook.com.tr>
2022-11-06 20:12:49 +0300

    Header component export

--- apps/home-page-mfe/src/app/app.module.ts
+++ apps/home-page-mfe/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HeaderModule } from '@mf/header';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule,HeaderModule],
+  imports: [BrowserModule,HeaderModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/header/src/lib/header.module.ts
+++ libs/header/src/lib/header.module.ts
@@ -5,5 +5,8 @@ import { HeaderComponent } from './header/header.component';
 @NgModule({
   imports: [CommonModule],
   declarations: [HeaderComponent],
+  exports: [
+    HeaderComponent,
+  ],
 })
 export class HeaderModule {}
